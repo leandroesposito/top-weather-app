180216f0cbe5fb4e426e609e685aa1e1a1bb9621
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-23 12:22:23 -0300

    Add example ussage of getWheatherInformation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./style.css";
+import getWheatherInformation from "./data-fetcher";
+
+(function () {
+  const promise = getWheatherInformation("Westport ireland", "metric");
+  console.log(promise);
+  promise
+    .then((data) => {
+      if ("error" in data) {
+        console.error(data.error);
+      } else {
+        console.log("Datos recibidos:", data);
+      }
+    })
+    .catch((error) => {
+      console.log("Error capturado en el último catch:", error.message);
+    });
+})();
