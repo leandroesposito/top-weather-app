6e6e3124a528426adda953e6d9022a2e4f2a1a42
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-26 19:21:09 -0300

    Remove old promise code sample

--- src/index.js
+++ src/index.js
@@ -53,19 +53,3 @@ import {
 
   renderIndications();
 })();
-
-(function () {
-  const promise = getWheatherInformation("Westport ireland", "metric");
-  console.log(promise);
-  promise
-    .then((data) => {
-      if ("error" in data) {
-        console.error(data.error);
-      } else {
-        console.log("Datos recibidos:", data);
-      }
-    })
-    .catch((error) => {
-      console.log("Error capturado en el último catch:", error.message);
-    });
-});
