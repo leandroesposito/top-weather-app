ef35aa3437286442066ccc352d752864c925d98a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-26 18:55:12 -0300

    Create renderer functions

--- src/render-controller.js
+++ src/render-controller.js
@@ -1,10 +1,26 @@
-import { createWeatherContent } from "./content-generator";
+import {
+  createWeatherContent,
+  createErrorContent,
+  createIndicationsContent,
+} from "./content-generator";
 
-export function renderData(data, unit) {
+function cleanBodyAndAppendChild(element) {
   const body = document.querySelector(".body");
   body.innerHTML = "";
+  body.appendChild(element);
+}
 
+export function renderData(data, unit) {
   const container = createWeatherContent(data, unit);
+  cleanBodyAndAppendChild(container);
+}
 
-  body.appendChild(container);
+export function renderError(error) {
+  const container = createErrorContent(error);
+  cleanBodyAndAppendChild(container);
+}
+
+export function renderIndications() {
+  const container = createIndicationsContent();
+  cleanBodyAndAppendChild(container);
 }
